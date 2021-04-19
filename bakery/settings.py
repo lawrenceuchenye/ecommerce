af8afc529dfac988912e1f6e7cf2b2b9e89dd75c
lawrenceuchenye <lawuche29@gmail.com>
2021-04-19 20:37:50 +0100

    debugging email

--- bakery/settings.py
+++ bakery/settings.py
@@ -158,10 +158,11 @@ STRIPE_API_HIDDEN_KEY="sk_test_51IfRKVLh9kwXHjLMb5fSXC4ZL6W3HQEAhxB9wZUZQv9GyGPK
 
 LOGOUT_REDIRECT_URL="home"
 
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_PORT="587"
+EMAIL_PORT=587
 
 EMAIL_HOST_USER="divinebakery81@gmail.com"
 EMAIL_HOST_PASSWORD="DivineBakery81@gmail.com"
 EMAIL_USE_TSL=True
-     
+
