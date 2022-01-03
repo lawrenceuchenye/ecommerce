0cba12fb365eed3d0d1d669f69b24a5f7c08b1a1
lawrenceuchenye <80986362+lawrenceuchenye@users.noreply.github.com>
2022-01-03 23:12:44 +0100

    Update settings.py

--- bakery/settings.py
+++ bakery/settings.py
@@ -167,6 +167,6 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 
 EMAIL_HOST_USER="divinebakery81@gmail.com"
-EMAIL_HOST_PASSWORD="DivineBakery81@gmail.com"
+EMAIL_HOST_PASSWORD="***********"
 
 EMAIL_USE_TLS=True
